528f669129c0552e825e88c86c57af2c980dedd7
Universal User1 <universaluser1@palanhare.onmicrosoft.com>
2025-09-20 10:09:44 +0530

    old files removed

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -31,11 +31,13 @@ module "sql_server" {
     admin_password = "P@ssword1234"
     tags = local.common_tags
 }
-
 module "sql_db" {
-    depends_on = [module.sql.server]
+    depends_on = [module.sql_server]
+    source = "../../modules/azurerm_sql_database"
     sql_db_name = "sqldb_dev_todoapp"
-    server_id = module.sql_sever.server_id
+    rg_name = "rg-dev-todoapp"
+    location = "centralindia"
+    server_id = module.sql_server.server_id
     max_size_gb = "2"
     tags = local.common_tags
 }
@@ -46,6 +48,24 @@ module "aks" {
     rg_name = "rg-dev-todoapp"
     location = "centralindia"
     dns_prefix = "aks_dev_todoapp"
+    tags = local.common_tags
+}
+module "storage_account" {
+    depends_on = [module.rg]
+    source = "../../modules/azurerm_storage_account"
+    sa_name = "staddevtodoapp"
+    rg_name = "rg-dev-todoapp"
+    location = "centralindia"
+    tags = local.common_tags
+}
+module "key_vault" {
+    depends_on = [module.rg]
+    source = "../../modules/azurerm_key_vault"
+    kv_name = "kv-dev-todoapp"
+    rg_name = "rg-dev-todoapp"
+    location = "centralindia"
+    tenant_id = "your-tenant-id"
+    sku_name = "standard"
     tags = local.common_tags
 }
 
